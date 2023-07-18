13cde55b97e636b09e5dbdb7ab73bf3a8f241dca
Jack Smith <jack_smith21554@yahoo.com>
2023-07-18 13:13:36 -0400

    Created general file structure and began adding generic files. Began working on index.js and App.js files.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// Import Components
+
+
+function App() {
+    return (
+        <div className='portfolio-app'>
+            {/* add components */}
+        </div>
+    );
+}
+
+export default App;
